02339c551b38bf9ec9cb0f2ac02a3d619d88efc6
Jeff Campbell <jeff@zapdot.com>
2019-06-10 10:23:32 +0200

    v0.1.0

---
+++ Slate/Scripts/Editor/VersionConstants.cs
@@ -0,0 +1,41 @@
+/*
+Slate
+Copyright (C) Jeff Campbell - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Jeff Campbell <mirraraenn@gmail.com>, 2022
+*/
+
+namespace JCMG.Slate.Editor
+{
+    /// <summary>
+    /// Version info for this library.
+    /// </summary>
+    public static class VersionConstants
+    {
+        /// <summary>
+        /// The semantic version
+        /// </summary>
+        public const string VERSION = "0.1.0";
+
+        /// <summary>
+        /// The branch of GIT this package was published from.
+        /// </summary>
+        public const string GIT_BRANCH = "";
+
+        /// <summary>
+        /// The current GIT commit hash this package was published on.
+        /// </summary>
+        public const string GIT_COMMIT = "";
+
+        /// <summary>
+        /// The UTC human-readable date this package was published at.
+        /// </summary>
+        public const string PUBLISH_DATE = "Saturday, 03 December 2022";
+
+        /// <summary>
+        /// The UTC time this package was published at.
+        /// </summary>
+        public const string PUBLISH_TIME = "12/03/2022 17:00:54";
+    }
+}
---
+++ Slate/Scripts/Reactions/UIScreenTransitionController.cs
@@ -0,0 +1,131 @@
+﻿/*
+Slate
+Copyright (C) Jeff Campbell - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Jeff Campbell <mirraraenn@gmail.com>, 2022
+*/
+
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace JCMG.Slate
+{
+	/// <summary>
+	/// A component that handles any UX reactions to a panel being shown or hidden.
+	/// </summary>
+	[RequireComponent(typeof(UIScreen))]
+	[AddComponentMenu("JCMG/Slate/UIScreenTransitionController")]
+	public sealed class UIScreenTransitionController : MonoBehaviour
+	{
+		/// <summary>
+		/// The type of transition object that should be assigned.
+		/// </summary>
+		private enum TransitionSourceType
+		{
+			MonoBehaviour = 0,
+			ScriptableObject = 1
+		}
+
+		/// <summary>
+		/// Returns true if using a scriptable transition for show, otherwise false.
+		/// </summary>
+		internal bool IsUsingShowScriptableTransition =>
+			_showTransitionSourceType == TransitionSourceType.ScriptableObject;
+
+		/// <summary>
+		/// Returns true if using a scriptable transition for hide, otherwise false.
+		/// </summary>
+		internal bool IsUsingHideScriptableTransition =>
+			_showTransitionSourceType == TransitionSourceType.ScriptableObject;
+
+		[BoxGroup(RuntimeConstants.UI_REFS)]
+		[SerializeField, Required]
+		private UIScreen _screen;
+
+		[Space(5)]
+		[BoxGroup("Show Configuration")]
+		[SerializeField]
+		private TransitionSourceType _showTransitionSourceType;
+
+		[BoxGroup("Show Configuration")]
+		[ShowIf("IsUsingShowScriptableTransition")]
+		[SerializeField]
+		private ScreenTransitionBase showScreenTransition;
+
+		[BoxGroup("Show Configuration")]
+		[HideIf("IsUsingShowScriptableTransition")]
+		[SerializeField]
+		private BehaviourScreenTransitionBase showBehaviourScreenTransition;
+
+		[BoxGroup("Hide Configuration")]
+		[SerializeField]
+		private TransitionSourceType _hideTransitionSourceType;
+
+		[BoxGroup("Hide Configuration")]
+		[ShowIf("IsUsingHideScriptableTransition")]
+		[SerializeField]
+		private ScreenTransitionBase hideScreenTransition;
+
+		[BoxGroup("Hide Configuration")]
+		[HideIf("IsUsingHideScriptableTransition")]
+		[SerializeField]
+		private BehaviourScreenTransitionBase hideBehaviourScreenTransition;
+
+		/// <summary>
+		/// Plays a transition to show the attached <see cref="UIScreen"/> instance; upon completion,
+		/// <paramref name="onShowComplete"/> will be invoked.
+		/// </summary>
+		public void AnimateShowReaction(ScreenTransitionCallback onShowComplete)
+		{
+			var showScreenReaction = GetShowScreenTransition();
+			if (showScreenReaction != null)
+			{
+				showScreenReaction.PlayTransition(onShowComplete);
+			}
+			else
+			{
+				onShowComplete?.Invoke();
+			}
+		}
+
+		/// <summary>
+		/// Plays a transition to hide the attached <see cref="UIScreen"/> instance; upon completion,
+		/// <paramref name="onHideComplete"/> will be invoked.
+		/// </summary>
+		public void AnimateHideReaction(ScreenTransitionCallback onHideComplete)
+		{
+			var hideScreenReaction = GetHideScreenTransition();
+			if (hideScreenReaction != null)
+			{
+				hideScreenReaction.PlayTransition(onHideComplete);
+			}
+			else
+			{
+				onHideComplete?.Invoke();
+			}
+		}
+
+		/// <summary>
+		/// Returns the <see cref="IScreenTransition"/> in-use for this instance for showing the attached
+		/// <see cref="UIScreen"/>.
+		/// </summary>
+		private IScreenTransition GetShowScreenTransition()
+		{
+			return _showTransitionSourceType == TransitionSourceType.ScriptableObject
+				? showScreenTransition
+				: showBehaviourScreenTransition;
+		}
+
+		/// <summary>
+		/// Returns the <see cref="IScreenTransition"/> in-use for this instance for hiding the attached
+		/// <see cref="UIScreen"/>.
+		/// </summary>
+		private IScreenTransition GetHideScreenTransition()
+		{
+			return _hideTransitionSourceType == TransitionSourceType.ScriptableObject
+				? hideScreenTransition
+				: hideBehaviourScreenTransition;
+		}
+	}
+}
